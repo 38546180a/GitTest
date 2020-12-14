587c35e6070356f4571300f3ff6d6bf4cc1fe32f
Zhang38546180 <38546180@qq.com>
2020-12-14 17:09:10 +0800

    大娃

---
+++ java.java
@@ -0,0 +1,8 @@
+public class java {
+    public static void main(String[] args) {
+        System.out.println("hollw1");
+        System.out.println("hollw1");
+        System.out.println("hollw1");
+        System.out.println("hollw1");
+    }
+}
